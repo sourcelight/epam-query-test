9164c18f600f88939ec3b11172fded546528a793
Riccardo_Bruno <Riccardo_Bruno@host.docker.internal>
2021-11-24 23:17:00 +0100

    fix, update

--- src/main/java/org/query/repository/T1Repository.java
+++ src/main/java/org/query/repository/T1Repository.java
@@ -10,12 +10,13 @@ import org.springframework.stereotype.Repository;
 @Repository("t1Repository")
 public interface T1Repository extends JpaRepository<T1, Long> {
 	
-	@Query(value="select r.a a, r.s s from "
+	@Query(value="select a , s from "
+			+ "(select r.a a, r.s s from "
 			+ "(SELECT  p.a a, COALESCE(sum(p.x * p.y * p.z ),0) s  FROM "
-			+ "(select * from t1 u1 LEFT OUTER JOIN  (select u2.b ,u2.y, u3.c,u3.z from t2 u2, t3 u3) d "
+			+ "(select * from t1 u1 LEFT OUTER JOIN  (select u2.b ,u2.y, u3.c,u3.z from t2 u2, t3 u3) d  "
 			+ "on (u1.a < d.b + d.c) ) p group BY p.a ) r "
-			+ "where ROWNUM <= 10 "
-			+ "ORDER BY s DESC " , nativeQuery = true)
+			+ "ORDER BY s DESC) ex  "
+			+ "where rownum <=10 " , nativeQuery = true)
 	List <Projection>extractResults();
 	
 	
--- src/test/resources/tables.sql
+++ src/test/resources/tables.sql
@@ -96,6 +96,17 @@ where ROWNUM <= 10
 ORDER BY s DESC;
 
 
+--last version, rownum above used incorrectly
+
+select a , s from 
+(select r.a a, r.s s from 
+(SELECT  p.a a, COALESCE(sum(p.x * p.y * p.z ),0) s  FROM
+(select * from t1 u1 LEFT OUTER JOIN  (select u2.b ,u2.y, u3.c,u3.z from t2 u2, t3 u3) d 
+on (u1.a < d.b + d.c) ) p group BY p.a ) r
+ORDER BY s DESC) ex 
+where rownum <=10;
+
+
 --questions:
 1)The other joins are full outer join without any common attribute ?
 2)Should I use an embedded DB or an external ?
