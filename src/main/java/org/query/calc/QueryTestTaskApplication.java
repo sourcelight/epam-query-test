c26b27c0f704afbe6f5df3d548cebdd37a354dee
Riccardo_Bruno <Riccardo_Bruno@host.docker.internal>
2021-11-26 00:01:04 +0100

    cleaning

--- src/main/java/org/query/calc/QueryTestTaskApplication.java
+++ src/main/java/org/query/calc/QueryTestTaskApplication.java
@@ -2,16 +2,8 @@ package org.query.calc;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.domain.EntityScan;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.PropertySource;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
-@ComponentScan({"org.query.model.entity","org.query.repository"})
-@EntityScan("org.query")
-@EnableJpaRepositories(basePackages  = "org.query.repository")
-@PropertySource("classpath:application.properties")
 public class QueryTestTaskApplication {
 
 		
--- src/test/java/org/query/calc/QueryTestTaskApplicationTests.java
+++ src/test/java/org/query/calc/QueryTestTaskApplicationTests.java
@@ -1,31 +1,13 @@
 package org.query.calc;
 
 import org.junit.jupiter.api.Test;
-import org.query.model.entity.T1;
-import org.query.repository.T1Repository;
-import org.query.repository.T2Repository;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
 
-@SpringBootTest
-class QueryTestTaskApplicationTests {
-	
-	@Autowired
-	@Qualifier("t1Repository")
-	private T1Repository t1Repository;
-	
-	@Autowired
-	@Qualifier("t2Repository")
-	private T2Repository t2Repository;
-
+@SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
+class QueryTestTaskApplicationTests {	
 	@Test
-	void contextLoads() {
-		
-		System.out.println("test");
-		T1 t1 = new T1();
-		t1Repository.save(t1);
-		System.out.println("test dopo ");
+	void contextLoads() {		
 	}
 
 }
