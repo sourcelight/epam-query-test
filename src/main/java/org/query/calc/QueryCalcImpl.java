8ffcab3c1ebbec63f526140ab8612077a54613db
Riccardo_Bruno <Riccardo_Bruno@EPITMILW0029>
2021-12-01 15:26:05 +0100

    fixed object creation

--- src/main/java/org/query/calc/QueryCalcImpl.java
+++ src/main/java/org/query/calc/QueryCalcImpl.java
@@ -54,20 +54,22 @@ public class QueryCalcImpl implements QueryCalc {
 		
 		List<String[]> parsedRows = null;
 		
+		Double[] myDoubleArray = new Double[2];
+		
 		//loading data files from file t1 in table T1
 		parsedRows = loadTables(t1);		
 		parsedRows.stream().map(row -> row[0].split(" ")).
-		map(arr->{double[] myDoubleArray = {Double.parseDouble(arr[0]),Double.parseDouble(arr[1]) };return myDoubleArray; }).forEach(d->t1Repository.save(new T1(null, d[0], d[1])));
+		map(arr->{myDoubleArray[0]=Double.parseDouble(arr[0]);myDoubleArray[1]=Double.parseDouble(arr[1]);return myDoubleArray; }).forEach(d->t1Repository.save(new T1(null, d[0], d[1])));
 		
 		//loading data files from file t2 in table T2
 		parsedRows = loadTables(t2);		
 		parsedRows.stream().map(row -> row[0].split(" ")).
-		map(arr->{double[] myDoubleArray = {Double.parseDouble(arr[0]),Double.parseDouble(arr[1]) };return myDoubleArray; }).forEach(d->t2Repository.save(new T2(null, d[0], d[1])));
+		map(arr->{myDoubleArray[0]=Double.parseDouble(arr[0]);myDoubleArray[1]=Double.parseDouble(arr[1]);return myDoubleArray; }).forEach(d->t2Repository.save(new T2(null, d[0], d[1])));
 
 		//loading data files from file t3 in table T3
 		parsedRows = loadTables(t3);		
 		parsedRows.stream().map(row -> row[0].split(" ")).
-		map(arr->{double[] myDoubleArray = {Double.parseDouble(arr[0]),Double.parseDouble(arr[1]) };return myDoubleArray; }).forEach(d->t3Repository.save(new T3(null, d[0], d[1])));
+		map(arr->{myDoubleArray[0]=Double.parseDouble(arr[0]);myDoubleArray[1]=Double.parseDouble(arr[1]);return myDoubleArray; }).forEach(d->t3Repository.save(new T3(null, d[0], d[1])));
 
 		//Extracting result data from DB
 		List<Projection> list = t1Repository.extractResults();	
